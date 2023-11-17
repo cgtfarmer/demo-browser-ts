75e36ee86b6d0716721c4b53bddc4eeb9a2a1938
Christian Farmer <christian.farmer@paymentspring.com>
2023-11-17 17:53:47 -0600

    Cleanup

--- jest.config.js
+++ jest.config.js
@@ -3,39 +3,12 @@ const { compilerOptions } = require('./tsconfig')
 
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
+  preset: 'ts-jest',
+  testEnvironment: 'node',
   roots: ['<rootDir>/src'],
-  // modulePaths: [compilerOptions.baseUrl],
-  // modulePaths: ['<rootDir>/src/assets/app/'],
-  // modulePaths: ['node_modules', '<rootDir>/src/assets/app/'],
-  // modulePaths: ['node_modules', '<rootDir>/src/assets/app/'],
-  // modulePaths: ['<rootDir>/app'],
-  // moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths /*, { prefix: '<rootDir>/' } */),
-  // moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/src/assets/app' }),
-  // moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
-  // moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
-  // transform: {
-  //     '^.+\\.tsx?$': 'ts-jest'
-  // },
-  // moduleDirectories: ['node_modules', './src/assets/app'],
-  // moduleDirectories: ['<rootDir>/app', 'node_modules'],
-  // moduleDirectories: ['node_modules', '<rootDir>/src/assets/app/'],
-  // moduleDirectories: ['node_modules', './app'],
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
-  // moduleNameMapper: {
-  //   // '^@/(.*)$': '<rootDir>/src/assets/app/$1'
-  //   '^@/(.*)$': '<rootDir>/src/$1'
-  // },
-  // moduleNameMapper: {
-  //   // '^@/(.*)$': '<rootDir>/src/assets/app/$1'
-  //   '^@/(.*)$': '$1'
-  // },
-  // moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/src/assets/app' }),
   testMatch: [
-    // "**/__tests__/**/*.[jt]s?(x)",
-    // "**/?(*.)+(spec|test).[tj]s?(x)"
-    "**/__tests__/**/*.[jt]s?(x)",
-    "**/?(*.)+(spec|test).[jt]s?(x)"
+    '**/__tests__/**/*.[jt]s?(x)',
+    '**/?(*.)+(spec|test).[jt]s?(x)'
   ],
 }
