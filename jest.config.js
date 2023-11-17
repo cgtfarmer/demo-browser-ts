401332dfc929440b365cc18a7da55931b10be8d4
Christian Farmer <christian.gt.farmer@gmail.com>
2023-11-17 12:27:19 -0600

    Updates

--- jest.config.js
+++ jest.config.js
@@ -5,20 +5,27 @@ const { compilerOptions } = require('./tsconfig')
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  roots: ['<rootDir>/src/assets/app'],
+  // roots: ['<rootDir>'],
   // modulePaths: [compilerOptions.baseUrl],
   // modulePaths: ['<rootDir>/src/assets/app/'],
   // modulePaths: ['node_modules', '<rootDir>/src/assets/app/'],
+  // modulePaths: ['node_modules', '<rootDir>/src/assets/app/'],
+  // modulePaths: ['<rootDir>/app'],
   // moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths /*, { prefix: '<rootDir>/' } */),
   // moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/src/assets/app' }),
-  // moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
+  // moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
   // transform: {
   //     '^.+\\.tsx?$': 'ts-jest'
   // },
   // moduleDirectories: ['node_modules', './src/assets/app'],
-  moduleDirectories: ['<rootDir>/src/assets/app', 'node_modules'],
+  // moduleDirectories: ['<rootDir>/app', 'node_modules'],
   // moduleDirectories: ['node_modules', '<rootDir>/src/assets/app/'],
+  // moduleDirectories: ['node_modules', './app'],
+  // moduleNameMapper: {
+  //   // '^@/(.*)$': '<rootDir>/src/assets/app/$1'
+  //   '^@/(.*)$': '<rootDir>/app/$1'
+  // },
   // moduleNameMapper: {
   //   // '^@/(.*)$': '<rootDir>/src/assets/app/$1'
   //   '^@/(.*)$': '$1'
